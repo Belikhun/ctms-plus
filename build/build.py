b758dc28691167520f64f04995084c91e6fb5d52
Belikhun <belivipro9x99@gmail.com>
2021-05-30 00:14:49 +0700

    🍋 ci: initial configuration

---
+++ build/build.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from json.decoder import JSONDecodeError
+from lib import ehook;
+from lib.log import log;
+
+import colorama
+from colorama import Fore
+colorama.init()
+log("OKAY", "Imported: colorama")
+
+import requests
+log("OKAY", "Imported: requests")
+
+import json
+log("OKAY", "Imported: json")
+
+import os
+log("OKAY", "Imported: os")
+
+def logStatus(text, status, overWrite = False):
+	statusText = [f"{Fore.RED}✗ ERRR", f"{Fore.YELLOW}● WAIT", f"{Fore.GREEN}✓ OKAY"]
+	logStatus = ["ERRR", "INFO", "OKAY"]
+
+	log(logStatus[status + 1], "{:48}{}{}".format(text, statusText[status + 1], Fore.RESET), resetCursor = (not overWrite))
+
+
+logStatus("Lấy Thông Tin Dự Án", 0)
+metadata = {}
+with open("metadata.json", "r", encoding="utf-8") as file:
+	try:
+		metadata = json.loads(file.read())
+	except JSONDecodeError:
+		logStatus("Lấy Thông Tin Dự Án", -1, True)
+		log("ERRR", "{} → Reason: {}Lỗi giải mã JSON (metadata.json)".format(Fore.WHITE, Fore.LIGHTBLACK_EX))
+		exit(-1)
+
+logStatus("Lấy Thông Tin Dự Án", 1, True)
+
+
+logStatus("Cập Nhật Danh Sách Người Đóng Góp", 0)
+contributors = requests.get("https://api.github.com/repos/belivipro9x99/ctms-plus/contributors")
+contributorsData = contributors.json()
+
+if (contributors.status_code != 200):
+	logStatus("Cập Nhật Danh Sách Người Đóng Góp", -1, True)
+	log("ERRR", "{} → Reason: ({}) {}".format(Fore.LIGHTRED_EX, str(contributors.status_code), contributorsData["message"]))
+	exit(-1)
+
+logStatus("Cập Nhật Danh Sách Người Đóng Góp", 1)
+for user in contributorsData:
+	log("DEBG", "User {}: {} contributions".format(user["login"], user["contributions"]))
+	metadata["contributors"][user["login"]] = {
+		"contributions": user["contributions"]
+	}
+
+if (os.environ.get("CI")):
+	log("INFO", "CI Environment Detected! Updaing Optional Data")
+	metadata["branch"] = os.environ.get("GITHUB_REF")
+	metadata["version"] += f".{os.environ.get('GITHUB_RUN_ID')}/{os.environ.get('GITHUB_SHA')[0:6]}"
+
+	log("DEBG", "branch = " + metadata["branch"])
+	log("DEBG", "version = " + metadata["version"])
+
+
+logStatus("Lưu Thông Tin Dự Án", 0)
+with open("metadata.json", "w", encoding="utf-8") as file:
+	file.write(json.dumps(metadata, indent=4))
+
+logStatus("Lưu Thông Tin Dự Án", 1, True)
+exit(0)

2bc2a30c962840a51bd9a38a551dc427c0052ce1
kakashi250802 <ngoduong37na@gmail.com>
2021-06-01 09:38:59 +0700

    thêm hiệu ứng vào trang dịch vụ payask

--- static/services/js/script.js
+++ static/services/js/script.js
@@ -1,18 +1,14 @@
 $(function() {
 	$(window).scroll(function(event) {
 		var pos_body = $('html,body').scrollTop();
-		var pos_pa2 = $('.pa-body .pa-word:nth-child(2)').offset().top; 
-		var pos_pa3 = $('.pa-body .pa-word:nth-child(3)').offset().top; 
-
-        console.log(pos_pa2);
-		if(pos_body>600){
-			$('.back-to-top').addClass('show-back-page');
+		if(pos_body>800){
+			$('.back-to-top').addClass('show-back-page go-top');
 		}
 		else{
-			$('.back-to-top').removeClass('show-back-page');
+			$('.back-to-top').removeClass('show-back-page go-top ');
+
 		}
 	});
-    if(pos_body)
 	$('.back-to-top').click(function(event) {
 		$('html,body').animate({
 			scrollTop: 0},

467366cb2275f4201baa2ead9b6b424294d56da8
Belikhun <belivipro9x99@gmail.com>
2021-08-18 18:55:01 +0700

    🍯 update library

--- assets/js/belibrary.js
+++ assets/js/belibrary.js
@@ -5,6 +5,80 @@
 //? |  Licensed under the MIT License. See LICENSE in the project root for license information.     |
 //? |-----------------------------------------------------------------------------------------------|
 
+const HTTP_STATUS_MESSAGES = {
+	100: `Continue`,
+	101: `Switching Protocols`,
+	102: `Processing`,
+
+	// 2×× Success
+	200: `OK`,
+	201: `Created`,
+	202: `Accepted`,
+	203: `Non-authoritative Information`,
+	204: `No Content`,
+	205: `Reset Content`,
+	206: `Partial Content`,
+	207: `Multi-Status`,
+	208: `Already Reported`,
+	226: `IM Used`,
+
+	// 3×× Redirection
+	300: `Multiple Choices`,
+	301: `Moved Permanently`,
+	302: `Found`,
+	303: `See Other`,
+	304: `Not Modified`,
+	305: `Use Proxy`,
+	307: `Temporary Redirect`,
+	308: `Permanent Redirect`,
+
+	// 4×× Client Error
+	400: `Bad Request`,
+	401: `Unauthorized`,
+	402: `Payment Required`,
+	403: `Forbidden`,
+	404: `Not Found`,
+	405: `Method Not Allowed`,
+	406: `Not Acceptable`,
+	407: `Proxy Authentication Required`,
+	408: `Request Timeout`,
+	409: `Conflict`,
+	410: `Gone`,
+	411: `Length Required`,
+	412: `Precondition Failed`,
+	413: `Payload Too Large`,
+	414: `Request-URI Too Long`,
+	415: `Unsupported Media Type`,
+	416: `Requested Range Not Satisfiable`,
+	417: `Expectation Failed`,
+	418: `I'm a teapot`,
+	421: `Misdirected Request`,
+	422: `Unprocessable Entity`,
+	423: `Locked`,
+	424: `Failed Dependency`,
+	426: `Upgrade Required`,
+	428: `Precondition Required`,
+	429: `Too Many Requests`,
+	431: `Request Header Fields Too Large`,
+	444: `Connection Closed Without Response`,
+	451: `Unavailable For Legal Reasons`,
+	499: `Client Closed Request`,
+
+	// 5×× Server Error
+	500: `Internal Server Error`,
+	501: `Not Implemented`,
+	502: `Bad Gateway`,
+	503: `Service Unavailable`,
+	504: `Gateway Timeout`,
+	505: `HTTP Version Not Supported`,
+	506: `Variant Also Negotiates`,
+	507: `Insufficient Storage`,
+	508: `Loop Detected`,
+	510: `Not Extended`,
+	511: `Network Authentication Required`,
+	599: `Network Connect Timeout Error`,
+}
+
 /**
  * An AJAX function designed for my API
  * @param	{Object}		param0		request data
@@ -80,6 +154,10 @@ function myajax({
 		xhr.addEventListener("progress", e => onDownload(e), false);
 
 		xhr.addEventListener("readystatechange", async function() {
+			let statusText = (typeof HTTP_STATUS_MESSAGES[this.status] !== "undefined")
+				? HTTP_STATUS_MESSAGES[this.status]
+				: this.statusText;
+
 			if (this.readyState === this.DONE) {
 				if (this.status === 0 && this.responseText === "") {
 					if (changeState === true)
@@ -102,9 +180,9 @@ function myajax({
 					}, {
 						color: flatc("red"),
 						text: `HTTP ${this.status}:`
-					}, this.statusText);
+					}, statusText);
 
-					let errorObj = { code: 1, description: `HTTP ${this.status}: ${this.statusText} (${method} ${url})`, data: { status: this.status, method, url } }
+					let errorObj = { code: 1, description: `HTTP ${this.status}: ${statusText} (${method} ${url})`, data: { status: this.status, method, url } }
 					error(errorObj);
 					reject(errorObj);
 
@@ -140,7 +218,7 @@ function myajax({
 							}, {
 								color: flatc("red"),
 								text: `HTTP ${this.status}:`
-							}, this.statusText, ` >>> ${response.description}`);
+							}, statusText, ` >>> ${response.description}`);
 	
 							if (this.status === 429 && response.code === 32 && reRequest === true) {
 								// Wait for :?unratelimited:?
@@ -164,7 +242,7 @@ function myajax({
 							}
 						}
 
-						let errorObj = { code: 3, description: `HTTP ${this.status}: ${this.statusText} (${method} ${url})`, data: response }
+						let errorObj = { code: 3, description: `HTTP ${this.status}: ${statusText} (${method} ${url})`, data: response }
 						error(errorObj);
 						reject(errorObj);
 
@@ -177,7 +255,7 @@ function myajax({
 
 					if (this.status >= 400) {
 						let code = `HTTP ${this.status}`;
-						let text = (this.statusText === "") ? "?Unknown statusText" : this.statusText;
+						let text = (statusText === "") ? "?Unknown statusText" : statusText;
 						let resData = response;
 
 						let header = this.getResponseHeader("output-json");
